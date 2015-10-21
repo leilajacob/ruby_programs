1e9b2b30e17b0cdf20d8a0ba16b21a77aeeeca86
Leila Jacob <leilajacob@me.com>
2015-10-21 10:48:02 -0400

    fixed bug

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,11 +1,21 @@
-students = %w[Aaron Janet Jamie Jude Miriam Ellen Robert Ashley Leila]
+students = %w[Aaron Janet Jamie Miriam Ellen Robert Julia Ashley Leila]
 
 students.shuffle!
 
 count = 0
+num_students = students.length 
 
-until count == (students.length - 3)
-	puts "#{students[count]} & #{students[count+1]}"
-	count += 2
+until count == num_students
+	if num_students % 2 == 1
+		if count < num_students - 3
+			puts "#{students[count]} & #{students[count+1]}"
+		else
+			puts "#{students[count]}, #{students[count+1]} & #{students[count+2]}"
+			break
+		end
+
+	else
+		puts "#{students[count]} & #{students[count+1]}"			
+	end
+		count += 2
 end
-puts "#{students[count]} & #{students[count+1]} & #{students[count+2]}"
