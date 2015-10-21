f554043dfa1ae5a459e4c8c090a4e448a7631fba
Leila Jacob <leilajacob@me.com>
2015-10-21 10:31:55 -0400

    paired students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,6 +1,11 @@
 students = %w[Aaron Janet Jamie Jude Miriam Ellen Robert Ashley Leila]
 
-students.shuffle
+students.shuffle!
 
-puts students
+count = 0
 
+until count == (students.length - 3)
+	puts "#{students[count]} & #{students[count+1]}"
+	count += 2
+end
+puts "#{students[count]} & #{students[count+1]} & #{students[count+2]}"
