b024e782ea85e90262f56849ba419c5be851f87c
Leila Jacob <leilajacob@me.com>
2015-10-21 10:56:45 -0400

    accepting input

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,9 +1,23 @@
-students = %w[Aaron Janet Jamie Miriam Ellen Robert Julia Ashley Leila]
+# students = %w[Aaron Janet Jamie Miriam Ellen Robert Julia Ashley Leila]
+students = []
+complete = false
 
 students.shuffle!
 
+puts "Who came to class today?"
+until complete == true
+	name = gets.chomp.downcase.capitalize
+	if name == "Done"
+		complete = true
+	else 
+		students.push(name)		
+	end
+end
+
 count = 0
 num_students = students.length 
+
+puts "Your teams are: "
 
 until count == num_students
 	if num_students % 2 == 1
