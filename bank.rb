239a3ff5880f3e8ed9883b7f3851db72e478b41d
Leila Jacob <leilajacob@me.com>
2015-10-22 08:52:19 -0400

    added main menu to bank

--- bank.rb
+++ bank.rb
@@ -17,12 +17,55 @@ class Account
 		@balance
 	end
 
-	def withdraw(amount)
+	def withdrawal(amount)
 		@balance -= amount
 	end
 
 	def deposit(amount)
 		@balance += amount
 	end
-
 end
+
+puts "Welcome to Tech Talent Bank"
+
+def main_menu
+# def create_account
+# end
+# def check_balance
+# end
+# def make_deposit
+# end
+# def make_withdrawal
+# end
+	puts "Please choose from the following selections: "
+	puts "------------------"
+	puts "1. Create an account"
+	puts "2. Check balance"
+	puts "3. Make a deposit"
+	puts "4. Make a withdrawal"
+	puts "5. End session"
+
+	choice = gets.chomp.to_i
+
+	if choice == 1
+		create_account
+	elsif choice == 2
+		check_balance
+	elsif choice == 3
+		make_deposit
+	elsif choice == 4
+		make_withdrawal
+	elsif choice == 5
+		end_session
+	else 
+		puts "That is not a valid selection."
+		main_menu
+	end
+end
+
+def end_session
+	puts "Thank you for banking with us!"
+	puts "Goodbye."
+end
+
+main_menu
